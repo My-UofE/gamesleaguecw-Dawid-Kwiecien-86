ced7562bb9779189d93510bb730ca9b9bd8944b5
Dawid-Kwiecien-86 <dk492@exeter.ac.uk>
2025-03-09 04:35:48 +0000

    Updated 'Player' class to be centered around an ArrayList of Players rather than IDs

--- src/gamesleague/Player.java
+++ src/gamesleague/Player.java
@@ -3,7 +3,7 @@ package gamesleague;
 import java.io.*;
 import java.util.ArrayList;
 
-public class Player {
+public class Player implements Serializable {
 
     private int id;
     private String email;
@@ -12,39 +12,92 @@ public class Player {
     private String phone;
 
     public Player(String email, String displayName, String name) {
-        this.id = getNextId();
         this.email = email;
         this.displayName = displayName;
         this.name = name;
+        setId(); // assigns the current player with the next available ID
+        addPlayerToList(this); // adds the current player to a list of all players
     }
 
     public Player(String email, String displayName, String name, String phone) {
-        this(email, displayName, name);
+        this.email = email;
+        this.displayName = displayName;
+        this.name = name;
         this.phone = phone;
+        setId();
+        addPlayerToList(this);
     }
 
-    public int getNextId() {
-        ArrayList<Integer> idList = getPlayerIds();
-        idList.add(idList.size());
-        serialisePlayerIds(idList);
-        return idList.size()-1;
+    public void addPlayerToList(Player player) {
+        ArrayList<Player> playerList = getPlayers();
+        playerList.add(this);
+        serialisePlayers(playerList); // serialising new list of players containing the current player into memory
     }
 
-    public static ArrayList<Integer> getPlayerIds() {
-        try (ObjectInputStream idIn = new ObjectInputStream(new FileInputStream("./src/gamesleague/save/PlayerIds.txt"))) {
-            return (ArrayList<Integer>) idIn.readObject();
+    public static ArrayList<Player> getPlayers() {
+        // De-serialising an ArrayList of Player objects from 'Players.ser' 
+        try (ObjectInputStream playerIn = new ObjectInputStream(new FileInputStream("./src/gamesleague/save/Players.ser"))) {
+            return (ArrayList<Player>) playerIn.readObject(); // reading value from bytestream
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+            // if the code reaches this point, the file is empty so we serialise an empty Player ArrayList to hold all Player objects
+            serialisePlayers(new ArrayList<Player>()); 
         }
-        return new ArrayList<Integer>();
+        return new ArrayList<Player>(); // if there are no players stored in 'Players.set', an empty list is returned
     }
 
-    public static void serialisePlayerIds(ArrayList<Integer> playerIds) {
-        try (ObjectOutputStream idOut = new ObjectOutputStream(new FileOutputStream("./src/gamesleague/save/PlayerIds.txt"))) {
-            idOut.writeObject(playerIds);
+    public static void serialisePlayers(ArrayList<Player> players) {
+        // Serialising the 'players' parameter to be stored in 'Players.ser'
+        try (ObjectOutputStream playerOut = new ObjectOutputStream(new FileOutputStream("./src/gamesleague/save/Players.ser"))) {
+            playerOut.writeObject(players); // writing value of 'players' to bytestream
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    // getters and setters
+    public int getId() {
+        return this.id;
+    }
+
+    // IDs are allocated automatically; should not be used for reassignment
+    private void setId() {
+        this.id = getPlayers().size(); // The next available ID is the size of the list of all players
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        serialisePlayers(getPlayers()); // Writes changes to object into memory
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+        serialisePlayers(getPlayers());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        serialisePlayers(getPlayers());
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+        serialisePlayers(getPlayers());
     }
 
 }
